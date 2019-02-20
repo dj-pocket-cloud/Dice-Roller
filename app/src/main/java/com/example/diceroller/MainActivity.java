3a0ab4ce2a6a79487a7c67ad8a15b3ecf58c47f5
dj-pocket-cloud <mstevens4@umassd.edu>
2019-02-19 22:18:38 -0500

    added guess implementation

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.diceroller;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable; // for EditText event handling
@@ -14,8 +15,16 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView sidevalue1;
     private TextView probability_value;
+    private EditText guessesvalue1;
+    private TextView guessresult;
+    private String guessText;
     private static final NumberFormat percentFormat = NumberFormat.getPercentInstance();
     private int sides = 6;
+    private int guess = -1; //set to -1 whenever there is no value in the textfield
+    private int result = 0;
+    private String positiveColor = "#FF00FF00"; //for these hex values, the first two numbers indicate the transparency of the color
+    private String negativeColor = "#FFFF0000"; //so FF at the beginning is fully visible, while 00 functionally hides the object with that color
+    private String transparentColor = "#00000000";
 
 
 
@@ -26,11 +35,33 @@ public class MainActivity extends AppCompatActivity {
 
         probability_value = (TextView) findViewById(R.id.probability_value);
         sidevalue1 = (TextView) findViewById(R.id.sidevalue1);
+        guessesvalue1 = (EditText) findViewById(R.id.guessesvalue1);
+        guessresult = (TextView) findViewById(R.id.guessresult);
 
         SeekBar sidebar1 = (SeekBar) findViewById(R.id.sidebar1);
         sidebar1.setOnSeekBarChangeListener(seekBarListener);
         sidesChanged();
-    }
+        clearGuessText();
+
+        //set integer guess variable whenever the guess textfield is changed
+        guessesvalue1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                guessText = guessesvalue1.getText().toString();
+                if (!guessText.matches("")) {
+                    guess = Integer.parseInt(guessText);
+                } else {
+                    guess = -1; //if textfield is left blank
+                }
+                clearGuessText();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {    }
+            @Override
+            public void afterTextChanged(Editable s) {  }
+        });
+        }
 
     private void sidesChanged(){
         double chance = (double) 1/sides;
@@ -44,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             sides = getNumSides(progress+1);
             sidesChanged();
+            clearGuessText();
         }
 
         @Override
@@ -64,4 +96,25 @@ public class MainActivity extends AppCompatActivity {
         else
             return 20;
     }
+
+    private void checkGuess() {
+        if (guess != -1) {
+            if (result == guess) {
+                //update guessresult with positive message
+                guessresult.setTextColor(Color.parseColor(positiveColor));
+                guessresult.setText("Correct!");
+            } else {
+                //update guessresult with negative message
+                guessresult.setTextColor(Color.parseColor(negativeColor));
+                guessresult.setText("Incorrect!");
+            }
+        }
+    }
+
+    private void clearGuessText() {
+        //hides guesstext when it's no longer relevant to current input (like when a value is changed by the user)
+        guessresult.setTextColor(Color.parseColor(transparentColor));
+    }
+
+
 }
